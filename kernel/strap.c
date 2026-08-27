cd85b6bbeedde52d43de5b5697584f40388c51cf
Daemon Wei <daemonwei123@gmail.com>
2026-08-27 19:58:38 +1000

    feat: handle user stack page faults

--- kernel/strap.c
+++ kernel/strap.c
@@ -54,7 +54,13 @@ void handle_user_page_fault(uint64 mcause, uint64 sepc, uint64 stval) {
       // dynamically increase application stack.
       // hint: first allocate a new physical page, and then, maps the new page to the
       // virtual address that causes the page fault.
-      panic( "You need to implement the operations that actually handle the page fault in lab2_3.\n" );
+      void *pa = alloc_page();
+      if (pa == 0)
+        panic("cannot allocate a physical page for user stack.\n");
+
+      user_vm_map((pagetable_t)current->pagetable,
+                  ROUNDDOWN(stval, PGSIZE), PGSIZE, (uint64)pa,
+                  prot_to_type(PROT_READ | PROT_WRITE, 1));
 
       break;
     default:
